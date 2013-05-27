702a39265838c3b3ee9d74cc06ebb156fa9a0391
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-27 16:29:47 +0900

    Add comment.

--- q0/solve.hs
+++ q0/solve.hs
@@ -3,7 +3,7 @@ import qualified L0
 import Class
 import L1
 
-
+-- 公理
 japaneseCurry :: NotHasLunch p Ramen -> NotHasLunch p Soba -> HasLunch p JapaneseCurry
 japaneseCurry (NotHasLunch ()) (NotHasLunch ()) = HasLunch ()
 
@@ -19,6 +19,7 @@ kantaHasRamen (HasLunch ()) = (NotHasLunch (), NotHasLunch ())
 chinpeiHasJapaneseCurry :: HasLunch Chinpei JapaneseCurry -> (NotHasLunch Kanta JapaneseCurry, NotHasLunch Tonchiki JapaneseCurry)
 chinpeiHasJapaneseCurry (HasLunch ()) = (NotHasLunch (), NotHasLunch ())
 
+-- 制約
 question :: (Person p, Lunch l) => p -> l -> Maybe (Either (NotHasLunch p l) (HasLunch p l))
 question p l
   | qr == Just True  = Just . Right $ HasLunch ()
@@ -26,25 +27,25 @@ question p l
   | otherwise        = Nothing
   where qr = L0.question (unSpecifyPerson p) (unSpecifyLunch l)
 
+-- 制約から得られた事実
+f0 :: Maybe (Either (NotHasLunch Chinpei Soba) (HasLunch Chinpei Soba))
+f0 =  question Chinpei' Soba'
 
-f0' :: Maybe (Either (NotHasLunch Kanta JapaneseCurry) (HasLunch Kanta JapaneseCurry))
-f0' =  question Kanta' JapaneseCurry'
+f1 :: Maybe (Either (NotHasLunch Kanta JapaneseCurry) (HasLunch Kanta JapaneseCurry))
+f1 =  question Kanta' JapaneseCurry'
 
-f1' :: Maybe (Either (NotHasLunch Kanta Soba) (HasLunch Kanta Soba))
-f1' =  question Kanta' Soba'
+f2 :: Maybe (Either (NotHasLunch Kanta Soba) (HasLunch Kanta Soba))
+f2 =  question Kanta' Soba'
 
-f2' :: Maybe (Either (NotHasLunch Chinpei Soba) (HasLunch Chinpei Soba))
-f2' =  question Chinpei' Soba'
-
-
+-- 事実から推論された結論
 solve :: (HasLunch Kanta Ramen, HasLunch Chinpei JapaneseCurry, HasLunch Tonchiki Soba)
 solve =
-  let (Just (Left f0)) = question Kanta' JapaneseCurry'
-      (Just (Left f1)) = question Kanta' Soba'
-      (Just (Left f2)) = question Chinpei' Soba'
-      kanta = ramen f1 f0
+  let (Just (Left chinpeiNotSoba)) = f0
+      (Just (Left kantaNotCurry))  = f1
+      (Just (Left kantaNotSoba))   = f2
+      kanta = ramen kantaNotSoba kantaNotCurry
       (tonchikiNotRamen, chinpeiNotRamen) = kantaHasRamen kanta
-      chinpei = japaneseCurry chinpeiNotRamen f2
+      chinpei = japaneseCurry chinpeiNotRamen chinpeiNotSoba
       (kantaNotCuryy, tonchikiNotCurry) = chinpeiHasJapaneseCurry chinpei
       tonchiki = soba tonchikiNotCurry tonchikiNotRamen
   in  (kanta, chinpei, tonchiki)
