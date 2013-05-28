aa900ab12c81d911d297b87962ff0cbf804a7c18
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-28 17:14:01 +0900

    Update example.

--- q0/Even3.hs
+++ q0/Even3.hs
@@ -4,9 +4,6 @@ import Even2
 
 data Even = Even (EvenPrime -> EvenPrime)
 
-evenValue :: Even -> Int
-evenValue (Even f) = evenPrimeValue (f zero')
-
 (<+>) :: Even -> Even -> Even
 (Even f) <+> (Even g) = Even (f . g)
 
@@ -16,6 +13,9 @@ zero =  Even id
 two :: Even
 two =  Even plus2'
 
+
+evenValue :: Even -> Int
+evenValue (Even f) = evenPrimeValue (f zero')
 
 instance Show Even where
   show = show . evenValue
