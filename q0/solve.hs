1b5786686b4a129d4a32eb9f1c275117a4c99af6
Kei Hibino <ex8k.hibino@gmail.com>
2013-05-28 19:48:32 +0900

    Update example.

--- q0/solve.hs
+++ q0/solve.hs
@@ -2,22 +2,19 @@
 import L1
 
 -- 制約から得られた事実
-f0 :: Maybe (Either (NotHasLunch Chinpei Soba) (HasLunch Chinpei Soba))
-f0 =  question Chinpei' Soba'
+chinpeiNotSoba :: NotHasLunch Chinpei Soba
+Just (Left chinpeiNotSoba) = question Chinpei' Soba'
 
-f1 :: Maybe (Either (NotHasLunch Kanta JapaneseCurry) (HasLunch Kanta JapaneseCurry))
-f1 =  question Kanta' JapaneseCurry'
+kantaNotCurry :: NotHasLunch Kanta JapaneseCurry
+Just (Left kantaNotCurry) =  question Kanta' JapaneseCurry'
 
-f2 :: Maybe (Either (NotHasLunch Kanta Soba) (HasLunch Kanta Soba))
-f2 =  question Kanta' Soba'
+kantaNotSoba :: NotHasLunch Kanta Soba
+Just (Left kantaNotSoba) = question Kanta' Soba'
 
 -- 事実から推論された結論
 solve :: (HasLunch Kanta Ramen, HasLunch Chinpei JapaneseCurry, HasLunch Tonchiki Soba)
 solve =
-  let (Just (Left chinpeiNotSoba)) = f0
-      (Just (Left kantaNotCurry))  = f1
-      (Just (Left kantaNotSoba))   = f2
-      kanta = ramen kantaNotSoba kantaNotCurry
+  let kanta = ramen kantaNotSoba kantaNotCurry
       (tonchikiNotRamen, chinpeiNotRamen) = kantaHasRamen kanta
       chinpei = japaneseCurry chinpeiNotRamen chinpeiNotSoba
       (kantaNotCuryy, tonchikiNotCurry) = chinpeiHasJapaneseCurry chinpei
